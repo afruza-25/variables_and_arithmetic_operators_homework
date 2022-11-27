35bae4da7407ff4a472f50749a25937684a4dea7
afruza-25 <118989989+afruza-25@users.noreply.github.com>
2022-11-27 13:16:01 +0500

    done
    added

---
+++ variables_and_arithmetic_operators_homework/arithmetic_op06.py
@@ -0,0 +1,10 @@
+# Create a variable "a"  and assign it the value of the int 7.
+a=7
+# Create a variable "b"  and assign it the value of the int 3.
+b=3
+# Create a variable "c"  and assign it the value of the int 5.
+c=5
+# Create a variable called "answer".
+answer=15
+# Find the sum of a, b and c then assign to "answer".
+a+b+c=answer
---
+++ variables_and_arithmetic_operators_homework/arithmetic_op09.py
@@ -0,0 +1,12 @@
+# Create a variable "a"  and assign it the value of the int 8.
+a=8
+# Create a variable "b"  and assign it the value of the int 3.
+b=3
+# Create a variable "c"  and assign it the value of the int 2.
+c=2
+# Create a variable "d"  and assign it the value of the int 4.
+d=4
+# Create a variable called 'answer'.
+answer=5
+# Find the expression value assign to 'answer'. a+b - cd .
+a+b-c d=answer
