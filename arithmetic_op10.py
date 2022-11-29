8b397a326e705e7af9f5f2e5afaff51aaf7fa511
afruza-25 <118989989+afruza-25@users.noreply.github.com>
2022-11-29 17:40:47 +0500

    changed

--- arithmetic_op10.py
+++ arithmetic_op10.py
@@ -1,7 +1,6 @@
 # Create a variable "n"  and assign it the value of the 4.
 n=4
-m=2
 # Create a variable called 'answer'.
-answer=m^n
+answer=2^n
 # Find the expression value assign to 'answer'. 2^n.
 print(answer)
